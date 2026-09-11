1b4c423f0b2605b1e43c9023db3a6900ea7737a2
ST10474729-Nithilan <padayacheenithilan@gmail.com>
2026-09-11 22:14:22 +0200

    Exception Handling

---
+++ ICE TASK 3 - ST10474729/Orderexception.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ordermessages;
+
+/**
+ *
+ * @author Nithilan
+ */
+
+public class Orderexception extends Exception
+{
+    
+    public Orderexception(String message)
+    {
+        super(message);
+      }
+}
